c9f972cce85cd86ec5ec78de503657987b926448
zoo1994 <composure1994@gmail.com>
2022-01-04 17:48:01 +0900

    s

--- src/com/sj/s1/method/StudentMain.java
+++ src/com/sj/s1/method/StudentMain.java
@@ -6,11 +6,21 @@ public class StudentMain {
 //	 makeStudent 호출
 //	 이름 번호 국어 영어 수학 출려
 		StudentUtil ut = new StudentUtil();
-		Student s = new Student();
-		
-		ut.makeStudent();
-	
+		Student s = ut.makeStudent();
 
+		Student[]ss = ut.makeStudents();
+		
+		for(int i=0;i<ss.length;i++) {
+			Student student = ss[i];
+			System.out.println("이름 : "+student.name);
+			System.out.println("번호 : "+student.number);
+			System.out.println("국어점수 : "+student.kor);
+			System.out.println("영어점수 : "+student.eng);
+			System.out.println("수학점수 : "+student.math);
+			
+			
+		}
+				
 	}
 
 }
--- src/com/sj/s1/method/StudentUtil.java
+++ src/com/sj/s1/method/StudentUtil.java
@@ -21,7 +21,32 @@ public class StudentUtil {
 		
 		return s;
 				
-		
+	}
+	public Student [] makeStudents() {
+		//학생의 수를 입력받음
+		//키보드로부터 이름 번호 국어 영어 수학 입력
+		//학생들의 정보를 리턴
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("학생 수를 입력하세요");
+		int num = sc.nextInt();
+		Student [] students = new Student[num];
+		for (int i =0;i<num;i++) {
+			Student s = new Student();
+			System.out.println("이름을 입력하세요");
+			s.name= sc.next();
+			System.out.println("번호를 입력하세요");
+			s.number = sc.nextInt();
+			System.out.println("국어 점수를 입력하세요");
+			s.kor = sc.nextInt();
+			System.out.println("영어 점수를 입력하세요");
+			s.eng = sc.nextInt();
+			System.out.println("수학 점수를 입력하세요");
+			s.math = sc.nextInt();
+			students[i] = s;
+			
+		}
+		return students;
 	}
 
 }
